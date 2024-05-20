d610fa3ea22e1d1bbdc4ef87337d610b9f49809e
SandeepKomal <sandeepkomalp@gmail.com>
2024-05-20 09:52:56 +0530

    Update QuizDao.java

--- src/main/java/com/komal/QuizApp/dao/QuizDao.java
+++ src/main/java/com/komal/QuizApp/dao/QuizDao.java
@@ -1,6 +1,6 @@
-package com.nadun.QuizApp.dao;
+package com.komal.QuizApp.dao;
 
-import com.nadun.QuizApp.model.Quiz;
+import com.komal.QuizApp.model.Quiz;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface QuizDao extends JpaRepository<Quiz, Integer> {
